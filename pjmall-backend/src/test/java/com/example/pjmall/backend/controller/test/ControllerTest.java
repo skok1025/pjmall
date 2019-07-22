5abf2c9ea905ee40ffbba5423f7384e7e74dcaf1
kickscar <kickscar@gmail.com>
2019-07-22 10:48:01 +0900

    ResourceServerConfig

--- pjmall-backend/src/test/java/com/example/pjmall/backend/controller/test/ControllerTest.java
+++ pjmall-backend/src/test/java/com/example/pjmall/backend/controller/test/ControllerTest.java
@@ -1,14 +1,20 @@
 package com.example.pjmall.backend.controller.test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -18,9 +24,26 @@ public class ControllerTest {
 	@Autowired
     MockMvc mockMvc;
 	
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+	
+	@Autowired
+	private FilterChainProxy springSecurityFilterChain;
+	
+	
+	@Before	
+	public void setUp() {
+		mockMvc = MockMvcBuilders.
+			webAppContextSetup(webApplicationContext)
+			.addFilters(springSecurityFilterChain)
+			.build();
+	}
+	
 	@Test
-	public void test() {
-		assertNotNull(mockMvc);
-		
+	public void testHelloUnauthorized() throws Exception {
+		mockMvc
+			.perform(get("/hello"))
+			.andDo(print())
+			.andExpect(status().isUnauthorized());
 	}
 }
